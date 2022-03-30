d59ae5be542c8d4d44b0e010529cbb1e1f891a50
Olga Zhabko <102178041+OlgaZhabko@users.noreply.github.com>
2022-03-30 20:53:26 +0300

    Add files via upload

---
+++ romannumbers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+'''This program asks to enter a Roman number and decodes it into an integer. If the entry is wrong it returns 0 or removes wrong symbols, leaving only MDCLXVI'''
+romans={'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
+def decode(numstr):
+    res=[romans[l] for l in numstr]
+    for i in range(1, len(res)):
+        if res[i-1]<res[i]:
+            res[i-1]=-res[i-1]    
+    return sum(res)
+romannumb=input('Enter a Roman number: ')
+romannumb=romannumb.upper()
+romannumb=''.join([let for let in romannumb if let in 'MDCLXVI'])
+print(f'The Arab number for Roman number {romannumb} is {decode(romannumb)}')     
