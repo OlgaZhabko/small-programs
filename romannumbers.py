7a0942131758c79038ad409d2b531e8aa67b84e7
Olga Zhabko <102178041+OlgaZhabko@users.noreply.github.com>
2022-04-03 15:40:09 +0300

    Update romannumbers.py

--- romannumbers.py
+++ romannumbers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-'''This program asks to enter a Roman number and decodes it into an integer. If the entry is wrong it returns 0 or removes wrong symbols, leaving only MDCLXVI'''
+'''This program asks to enter a Roman number and decodes it into an integer. If the entry is wrong it returns 0 or removes wrong symbols, leaving only symbols MDCLXVI'''
 romans={'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
 def decode(numstr):
     res=[romans[l] for l in numstr]
@@ -11,4 +11,4 @@ def decode(numstr):
 romannumb=input('Enter a Roman number: ')
 romannumb=romannumb.upper()
 romannumb=''.join([let for let in romannumb if let in 'MDCLXVI'])
-print(f'The Arab number for Roman number {romannumb} is {decode(romannumb)}')     
+print(f'The Arab number for the Roman number {romannumb} is {decode(romannumb)}')     
